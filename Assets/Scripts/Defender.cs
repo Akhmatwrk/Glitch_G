1b1ee46cabeffd2796cf7d6a82dd1d809b738970
Ruslan Akhmatdinov <infar@list.ru>
2020-03-19 17:24:30 +0300

    Finish work on trophy animation

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -8,4 +8,17 @@ public class Defender : MonoBehaviour
     [SerializeField] int starCost = 10;
 
 
+    public void AddStars(int amount)
+    {
+        StarDisplay starDisplay = FindObjectOfType<StarDisplay>();
+        starDisplay.AddStars(amount);
+    }
+
+    public void SpendStars(int amount)
+    {
+        StarDisplay starDisplay = FindObjectOfType<StarDisplay>();
+        starDisplay.SpendStars(amount);
+    }
+
 }
+
