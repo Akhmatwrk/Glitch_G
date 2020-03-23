6c77229a03a366c1a262f4a23eb31e30154e48b8
Ruslan Akhmatdinov <infar@list.ru>
2020-03-23 04:34:48 +0300

    Added Defenders Health, Attacking for Attackers, Fox

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,15 @@ public class Attacker : MonoBehaviour
 
     [Range (0f, 5f)]    [SerializeField] float movingSpeed = 0f;
 
+    GameObject currentTarget;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
+
     public void SetMovementSpeed(float movementSpeed)
     {
         movingSpeed = movementSpeed;
@@ -21,5 +26,31 @@ public class Attacker : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * movingSpeed);
+        CheckAnimationStatus();
+    }
+
+    private void CheckAnimationStatus()
+    {
+        if (!currentTarget)
+        {
+            animator.SetBool("isAttacking", false);
+        }
+
+    }
+
+    public void Attack (GameObject target)
+    {
+        animator.SetBool("isAttacking",true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) {return;}
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
